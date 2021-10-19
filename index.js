47995eec4432c17bb2ab3ee875d24b7724e17df2
Ved Prakash Dubey <vd8413@srmist.edu.in>
2021-10-19 12:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000
+const port = process.env.port || 3000;
 const handlebars = require("express-handlebars");
 const http = require("http").Server(app);
 // const io = require("socket.io")(http);
